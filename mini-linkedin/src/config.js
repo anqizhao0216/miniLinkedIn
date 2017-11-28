54dd47e5fd6939cbe93197afb12d425d96e648e4
anqizhao0216 <anqizhao0216@gmail.com>
2017-11-27 18:18:32 -0800

    Toast
    Toast

--- mini-linkedin/src/config.js
+++ mini-linkedin/src/config.js
@@ -1,5 +1,14 @@
 import axios from 'axios'
+import { Toast } from 'antd-mobile'
 
+//拦截请求
 axios.interceptors.request.use(function(config) {
-  
+  Toast.loading('loading', 0)
+  return config
+})
+
+//拦截响应
+axios.interceptors.response.use(function(config) {
+  Toast.hide()
+  return config
 })
