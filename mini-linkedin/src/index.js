1ce140af70c255745a15f5ed73eb562e2a309150
anqizhao0216 <anqizhao0216@gmail.com>
2017-11-28 22:10:00 -0800

    start
    start

--- mini-linkedin/src/index.js
+++ mini-linkedin/src/index.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
+import {createStore,applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {BrowserRouter,Route,Switch,Redirect} from 'react-router-dom'
 
 import reducers from './reducer'
-import Auth from './Auth'
-import Dashboard from './Dashboard'
 import './config'
-import 'antd-mobile/dist/antd-mobile.css'
 
 const store = createStore(reducers, compose(
   applyMiddleware(thunk),
@@ -27,11 +19,6 @@ console.log(store.getState())
 ReactDom.render(
   (<Provider store = {store}>
     <BrowserRouter>
-      <Switch>
-        <Route path='/login' exact component={Auth}></Route>
-        <Route path='/dashboard' component={Dashboard}></Route>
-        <Redirect to='/dashboard'></Redirect>
-      </Switch>
     </BrowserRouter>
    </Provider>),
    document.getElementById('root')
--- mini-linkedin/src/reducer.js
+++ mini-linkedin/src/reducer.js
@@ -1,8 +1,3 @@
-// 合并所有reducers 并且返回
-
 import { combineReducers } from 'redux'
-import { counter } from './index.redux'
-import { auth } from './Auth.redux'
 
-const reducers = combineReducers({counter, auth})
-export default reducers
+export default combineReducers({})
