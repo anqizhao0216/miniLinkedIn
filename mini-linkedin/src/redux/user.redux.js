436daa71a20899e2c9bbafa32b70310895e0acd8
anqizhao0216 <anqizhao0216@gmail.com>
2017-12-02 22:57:19 -0800

    error-msg

--- mini-linkedin/src/redux/user.redux.js
+++ mini-linkedin/src/redux/user.redux.js
@@ -13,14 +13,22 @@ const initState = {
 export function user(state = initState, action) {
   switch (action.type) {
     case REGISTER_SUCCESS:
-      return {...state, msg:'', isAuth:true,}
+      return {...state, msg:'', isAuth:true, ...action.payload}
     case ERROR_MSG:
+      return {...state, isAuth: false, msg: action.msg}
     default:
       return state
 
   }
 }
 
+function registerSuccess(data) {
+  return {type:REGISTER_SUCCESS, payload:data}
+}
+
+function errorMsg(msg) {
+  return {msg, type: ERROR_MSG}
+}
 export function register({user,pwd, repeatpwsd, type}) {
   if (!user||!pwd||!type) {
     return errorMsg('please type username and passward')
