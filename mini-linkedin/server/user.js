3291af9630ca7264ecc1567dd8d366767945a28f
anqizhao0216 <anqizhao0216@gmail.com>
2017-12-12 22:27:54 -0800

    login

--- mini-linkedin/server/user.js
+++ mini-linkedin/server/user.js
@@ -5,8 +5,19 @@ const User = model.getModel('user')
 const utils = require('utility')
 
 Router.get('/list', function(req, res) {
+    // User.remove({},function(err,doc){})
     User.find({}, function(err,doc) {
         return res.json(doc)
+    })
+})
+
+Router.post('/login', function(req, res) {
+    const {user, pwd} = req.body
+    User.findOne({user, pwd:md5Pwd(pwd)}, {'pwd': 0},function(err, doc) {
+      if (!doc) {
+        return res.json({code:1, msg:'wrong username or password'})
+      }
+      return res.json({code:0, data:doc})
     })
 })
 Router.post('/register', function(req, res) {
--- mini-linkedin/src/container/login/login.js
+++ mini-linkedin/src/container/login/login.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import Logo from '../../component/logo/logo'
+import {login} from '../../redux/user.redux'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
 import {List,InputItem,WingBlank,WhiteSpace,Button} from 'antd-mobile'
 
+@connect(
+  state => state.user,
+  {login}
+)
 class Login extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      user: '',
+      pwd: '',
+    }
     this.register = this.register.bind(this)
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+  handleChange(key, value) {
+    this.setState ({
+      [key]: value
+    })
+    // console.log(this.state.user)
+  }
+  handleLogin() {
+    this.props.login(this.state)
   }
   register() {
     this.props.history.push('./register')
@@ -13,16 +34,23 @@ class Login extends React.Component {
   render() {
     return (
       <div>
+        {this.props.redirectTo ? <Redirect to={this.props.redirectTo}/> : null}
         <Logo></Logo>
-        <h2>login</h2>
         <WingBlank>
           <List>
-            <InputItem>Username</InputItem>
+            {this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
+            <InputItem
+              onChange={v=>this.handleChange('user', v)}
+            >Username</InputItem>
             <WhiteSpace />
-            <InputItem>Password</InputItem>
+            <InputItem
+              onChange={v=>this.handleChange('pwd', v)}
+            >Password</InputItem>
           </List>
           <WhiteSpace />
-          <Button type="primary">Log In</Button>
+          <Button
+            onClick = {this.handleLogin}
+            type="primary">Log In</Button>
           <WhiteSpace />
           <Button onClick={this.register} type="primary">Register</Button>
         </WingBlank>
